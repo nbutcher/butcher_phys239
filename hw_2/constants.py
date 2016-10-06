a66b88bbe120162514eede538db6e889c9652284
nbutcher <butchernate@gmail.com>
2016-10-06 13:37:41 -0700

    Started the physical constants and unit conversion file.

---
+++ hw_2/constants.py
@@ -0,0 +1,9 @@
+#This file contains useful physical constants and unit conversions to be used in homework 2
+
+#Unit Conversions
+pc = 3.0857e18 #pc in cm CHECK THIS
+year = 31557600 #year in seconds
+
+#Physical Constants in cgs units
+c = 2997924.58
+
