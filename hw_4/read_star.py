2f23a6f7562a440f77c587d57db0db394fc2451a
nbutcher <butchernate@gmail.com>
2016-12-06 11:03:13 -0800

    Progress on read_star, not checked

--- hw_4/read_star.py
+++ hw_4/read_star.py
@@ -1,8 +1,39 @@
 import h5py
 import numpy as np
+from parameters import c, L_sun
 
 wavelist = []
 llist = []
 
+offset = 512
+end_off = offset + 10
 f = open('fig7b.txt')
+count = 0
 for line in f:
+    if (count <= 2):
+        count += 1
+        continue
+    wavestr = ''
+    for i in range(0,9):
+        wavestr += line[i]
+    wave = float(wavestr)
+    wavelist.append(wave)
+    lumstr = ''
+    for i in range(offset,end_off):
+        if (line[i] == '-'):
+            lumstr += '-' 
+        else:
+            lumstr += line[i]
+    #print lumstr
+    lum = float(lumstr)
+    llist.append(lum)
+
+f.close()
+A_in_Hz = c / 1e-10
+wavearr = np.array(wavelist) * 1e-4
+lumarr = np.array(llist) * L_sun / A_in_Hz
+
+outfile = h5py.File('Z020_Single.hdf5','w')
+wl = outfile.create_dataset('Wavelength',data=wavearr)
+luminosity = outfile.create_dataset('Luminosity',data=lumarr)
+outfile.close()
